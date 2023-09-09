03354a6415a1f1c8451d38241c646c24985bfa13
kirtijangid <97035935+kirtijangid@users.noreply.github.com>
2023-09-09 18:05:03 +0530

    increasing size of text and card

--- namer_app/lib/main.dart
+++ namer_app/lib/main.dart
@@ -74,7 +74,11 @@ class BigCard extends StatelessWidget {
       color: theme.colorScheme.primary,
       child: Padding(
         padding: const EdgeInsets.all(20),
-        child: Text(pair.asLowerCase),
+        child: Text(
+          pair.asLowerCase,
+        style: style,
+        semanticsLabel: "${pair.first} ${pair.second}"
+        ),
       ),
     );
   }
