b4aa0fa3445e17e2266a815b1fea0c8276d350b8
kirtijangid <97035935+kirtijangid@users.noreply.github.com>
2023-09-09 14:37:39 +0530

    changing the size and color of the card text

--- namer_app/lib/main.dart
+++ namer_app/lib/main.dart
@@ -63,13 +63,17 @@ class BigCard extends StatelessWidget {
     required this.pair,
   });
   final WordPair pair;
+
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
     return Card(
       color: theme.colorScheme.primary,
       child: Padding(
-        padding: const EdgeInsets.all(20.0),
+        padding: const EdgeInsets.all(20),
         child: Text(pair.asLowerCase),
       ),
     );
